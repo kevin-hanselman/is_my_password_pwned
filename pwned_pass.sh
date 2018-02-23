f2bae77a30d57c063fd43a70e322b793d95a60de
Kevin Hanselman <kevin.hanselman@gmail.com>
2018-02-22 21:03:24 -0500

    initial code commit

---
+++ pwned_pass.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -eo pipefail
+
+case "$1" in
+    -h|--help)
+        echo "usage: $(basename "$0") [password (or you will be prompted)]"
+        exit 0
+        ;;
+esac
+
+echo 'Reminder: This tool does not check password strength!'
+
+if [ -n "$1" ]; then
+    password="$1"
+else
+    echo -n "Type a password to check: "
+    read -r -s password
+    echo
+fi
+set -u
+
+hash=$(echo -n "$password" | sha1sum | awk '{print $1}')
+unset password
+hash_prefix=$(echo "$hash" | cut -c -5)
+hash_suffix=$(echo "$hash" | cut -c 6-)
+
+echo "Hash prefix: $hash_prefix"
+echo "Hash suffix: $hash_suffix"
+echo
+echo 'Looking up your password...'
+
+raw_count=$(curl -s "https://api.pwnedpasswords.com/range/$hash_prefix" | grep -i "$hash_suffix" | cut -d':' -f2 || true)
+# printf doesn't like some of the characters in the response text, but Python obliges
+count=$(python -c "print(int($raw_count))")
+
+printf "Your password appears in the Pwned Passwords database %u time(s).\\n" "$count"
+
+if [ "$count" -ge 100 ]; then
+    echo 'Your password is thoroughly pwned! DO NOT use this password for any reason!'
+elif [ "$count" -ge 20 ]; then
+    echo 'Your password is pwned! You should not use this password!'
+elif [ "$count" -gt 0 ]; then
+    echo 'Your password is pwned, but not ubiquitous. Use this password at your own risk!'
+elif [ "$count" -eq 0 ]; then
+    echo "Your password isn't pwned, but that doesn't necessarily mean it's secure!"
+fi
