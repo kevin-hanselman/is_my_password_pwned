e10df8e0c962f8692e9abced371cbd9469ca7cab
Kevin Hanselman <kevin.hanselman@gmail.com>
2019-01-23 21:56:41 -0500

    query multiple passwords, wipe password variable

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
 # Is my password pwned?
-Find out how often your password appears in [Troy Hunt's Pwned Passwords database](https://www.troyhunt.com/ive-just-launched-pwned-passwords-version-2/).
+Find out how often your password appears in [Troy Hunt's Pwned Passwords
+database](https://www.troyhunt.com/ive-just-launched-pwned-passwords-version-2/).
 
-Uses [the k-anonymity API](https://blog.cloudflare.com/validating-leaked-passwords-with-k-anonymity/) to keep your password from leaving your PC.
+This script uses [the k-anonymity
+API](https://blog.cloudflare.com/validating-leaked-passwords-with-k-anonymity/)
+to keep your password from leaving your computer, and it's written in concise
+Bash for transparency and portability.
 
-Written in concise Bash for transparency and portability.
+This script no longer accepts passwords via command line arguments. This usage
+would expose passwords in your [shell's
+history](https://www.tecmint.com/history-command-examples/), or to people in
+view of your computer screen. Clearly, you can still pipe passwords to the
+script, but doing so is strongly discouraged.
+
+## Usage Examples
 
 ```
 $ ./pwned_pass.sh
-Reminder: This tool does not check password strength!
-Type a password to check:
+
+REMINDER: This tool does not check password strength!
+
+Type a password and hit Enter (leave empty to exit):
 Hash prefix: 5baa6
 Hash suffix: 1e4c9b93f3f0682250b6cf8331b7ee68fd8
 
 Looking up your password...
-Your password appears in the Pwned Passwords database 3303003 time(s).
+Your password appears in the Pwned Passwords database 3645804 time(s).
 Your password is thoroughly pwned! DO NOT use this password for any reason!
+```
+
+```
+$ cat passwords.txt | ./pwned_pass.sh -q
+5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8    3645804
+8843d7f92416211de9ebb963ff4ce28125932878    11958
+5c6d9edc3a951cda763f650235cfc41a3fc23fe8    203116
 ```
--- pwned_pass.sh
+++ pwned_pass.sh
@@ -1,63 +1,107 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
 prog=$(basename "$0")
 
-fatal() {
-    echo >&2 "$prog: $1"
-    exit 1
+print_usage() {
+    echo "$prog"
+    echo
+    echo '  -h, --help      print this help text and exit'
+    echo '  -q, --quiet     suppress non-essential output'
+    echo
 }
 
-password=
-case "$1" in
-    -h|--help)
-        echo "usage: $prog [password (or you will be prompted)]"
-        exit 0
-        ;;
-    *)
-        password=$1
-        ;;
-esac
+prompt() {
+    if [ -z "$quiet" ]; then
+        echo
+        echo -n 'Type a password and hit Enter (leave empty to exit): '
+    fi
+}
 
-echo 'Reminder: This tool does not check password strength!'
+wipe_password_var() {
+    # obfuscate plaintext password, appeasing Bash by removing null characters
+    password=$(head -c 256 /dev/urandom | tr '\0' 'x')
+    unset password
+}
 
-if [ -z "$password" ]; then
-    printf "Type a password to check: "
-    read -r -s password
+check_if_pwned() {
+    # $password is a global variable so we can easily wipe it via a Bash trap
+    local password_hash
+    local hash_prefix
+    local hash_suffix
+    local count
+    password_hash=$(printf "%s" "$password" | openssl sha1 | awk '{print $NF}')
+
+    wipe_password_var
+
+    hash_prefix=$(echo "$password_hash" | cut -c -5)
+    hash_suffix=$(echo "$password_hash" | cut -c 6-)
+
+    if [ -z "$quiet" ]; then
+        echo "Hash prefix: $hash_prefix"
+        echo "Hash suffix: $hash_suffix"
+        echo
+        echo 'Looking up your password...'
+    fi
+
+    response=$(curl -fsS "https://api.pwnedpasswords.com/range/$hash_prefix")
+
+    count=$(echo "$response" \
+            | grep -i "$hash_suffix" \
+            | cut -d':' -f2 \
+            | grep -Eo '[0-9]+' \
+            || echo 0)
+
+    if [ -z "$quiet" ]; then
+        echo "Your password appears in the Pwned Passwords database $count time(s)."
+
+        if [ "$count" -ge 100 ]; then
+            echo 'Your password is thoroughly pwned! DO NOT use this password for any reason!'
+        elif [ "$count" -ge 20 ]; then
+            echo 'Your password is pwned! You should not use this password!'
+        elif [ "$count" -gt 0 ]; then
+            echo 'Your password is pwned, but not ubiquitous. Use this password at your own risk!'
+        elif [ "$count" -eq 0 ]; then
+            echo "Your password isn't pwned, but that doesn't necessarily mean it's secure!"
+        fi
+    else
+        echo "$password_hash    $count"
+    fi
+
+}
+
+trap wipe_password_var EXIT
+
+quiet=
+for arg in "$@"; do
+    case "$arg" in
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        -q|--quiet)
+            quiet=yes
+            ;;
+        *)
+            print_usage
+            echo "Unrecognized argument: '$arg'"
+            echo 'Note: For security reasons, this script no longer accepts passwords via command-'
+            echo 'line arguments. Please use the prompt or pipe a file into this script.'
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$quiet" ]; then
     echo
+    echo 'REMINDER: This tool does not check password strength!'
 fi
-set -u
+prompt
 
-hash=$(printf "%s" "$password" | openssl sha1 | awk '{print $NF}')
-    
-unset password
-hash_prefix=$(echo "$hash" | cut -c -5)
-hash_suffix=$(echo "$hash" | cut -c 6-)
-
-echo "Hash prefix: $hash_prefix"
-echo "Hash suffix: $hash_suffix"
-echo
-echo 'Looking up your password...'
-
-response=$(curl -s "https://api.pwnedpasswords.com/range/$hash_prefix") \
-    || fatal 'Failed to query the Pwned Passwords API'
-
-count=$( echo "$response" \
-             | grep -i "$hash_suffix" \
-             | cut -d':' -f2 \
-             | grep -Eo '[0-9]+' \
-             || echo 0)
-
-
-printf "Your password appears in the Pwned Passwords database %d time(s).\\n" "$count"
-
-if [ "$count" -ge 100 ]; then
-    echo 'Your password is thoroughly pwned! DO NOT use this password for any reason!'
-elif [ "$count" -ge 20 ]; then
-    echo 'Your password is pwned! You should not use this password!'
-elif [ "$count" -gt 0 ]; then
-    echo 'Your password is pwned, but not ubiquitous. Use this password at your own risk!'
-elif [ "$count" -eq 0 ]; then
-    echo "Your password isn't pwned, but that doesn't necessarily mean it's secure!"
-fi
+while read -r -s password; do
+    [ -n "$password" ] || exit 0
+    [ -n "$quiet" ] || echo
+    check_if_pwned
+    prompt
+done < /dev/stdin
